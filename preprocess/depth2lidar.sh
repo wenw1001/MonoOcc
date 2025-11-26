d8943aa5e4bbc3a2938f38d33ace0889b3727188
wenw1001 <winnie973314@gmail.comm>
2025-11-26 20:44:25 +0800

    Add custom modifications

--- preprocess/depth2lidar.sh
+++ preprocess/depth2lidar.sh
@@ -1,12 +1,12 @@
 set -e
 exeFunc(){
     num_seq=$1
-    python utils/depth2lidar.py --calib_dir  ~/MonoOcc/kitti/dataset/sequences/$num_seq \
-    --depth_dir ~/MonoOcc/preprocess/mobilestereonet/depth/sequences/$num_seq \
-    --save_dir ~/MonoOcc/preprocess/mobilestereonet/lidar/sequences/$num_seq
+    python utils/depth2lidar.py --calib_dir  /home/rvl/Desktop/wenwen/my_projects/MonoOcc/kitti/dataset/sequences/$num_seq \
+    --depth_dir /home/rvl/Desktop/wenwen/my_projects/MonoOcc/preprocess/mobilestereonet/depth/sequences/$num_seq \
+    --save_dir /home/rvl/Desktop/wenwen/my_projects/MonoOcc/preprocess/mobilestereonet/lidar/sequences/$num_seq
 
-    cp data_odometry_calib/sequences/$num_seq/calib.txt ~/MonoOcc/preprocess/mobilestereonet/lidar/sequences/$num_seq/
-    cp data_odometry_calib/sequences/$num_seq/poses.txt ~/MonoOcc/preprocess/mobilestereonet/lidar/sequences/$num_seq/
+    cp data_odometry_calib/sequences/$num_seq/calib.txt /home/rvl/Desktop/wenwen/my_projects/MonoOcc/preprocess/mobilestereonet/lidar/sequences/$num_seq/
+    cp data_odometry_calib/sequences/$num_seq/poses.txt /home/rvl/Desktop/wenwen/my_projects/MonoOcc/preprocess/mobilestereonet/lidar/sequences/$num_seq/
 }
 
 # mkdir -p $data_path/lidar
--- preprocess/image2depth.sh
+++ preprocess/image2depth.sh
@@ -3,7 +3,7 @@ exeFunc(){
     cd mobilestereonet
     baseline=$1
     num_seq=$2
-    CUDA_VISIBLE_DEVICES=1 python prediction.py --datapath ../kitti/dataset/sequences/$num_seq \
+    CUDA_VISIBLE_DEVICES=0 python prediction.py --datapath ../kitti/dataset/sequences/$num_seq \
     --testlist ./filenames/$num_seq.txt --num_seq $num_seq --loadckpt ./MSNet3D_SF_DS_KITTI2015.ckpt --dataset kitti \
     --model MSNet3D --savepath "./depth" --baseline $baseline
     cd ..
@@ -16,22 +16,22 @@ data_path=./preprocess
 
 # mkdir -p $data_path/depth
 # mkdir -p  /mobilestereonet/depth
-for i in {00..02}
+for i in 00 01 02
 do
     exeFunc 388.1823 $i     
 done
 
-for i in {03..03}
+for i in 03
 do
     exeFunc 389.6304 $i     
 done
 
-for i in {04..12}
+for i in 04 05 06 07 08 09 10 11 12
 do
     exeFunc 381.8293 $i     
 done
 
-for i in {13..21}
+for i in 13 14 15 16 17 18 19 20 21
 do
     exeFunc 388.1823 $i     
 done

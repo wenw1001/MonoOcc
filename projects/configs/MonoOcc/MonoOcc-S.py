ab362b58799ad6c87fa7fc893765b25ae1459b37
wenw1001 <winnie973314@gmail.comm>
2026-01-25 22:11:49 +0800

    修改config檔，儲存權重功能開啟

--- projects/configs/MonoOcc/MonoOcc-S.py
+++ projects/configs/MonoOcc/MonoOcc-S.py
@@ -233,5 +233,5 @@ log_config = dict(
        dict(type='TensorboardLoggerHook')
    ])
 
-checkpoint_config = None
-# checkpoint_config = dict(interval=2)
+# checkpoint_config = None
+checkpoint_config = dict(interval=1)
